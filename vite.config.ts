dd06bc5e5c7c93636ae6302758a2435cd7c3c8a1
rhychards <richartdmq@gmail.com>
2026-09-24 08:21:17 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 1000, // Eleva el límite permitido a 1000 kB para evitar alarmas visuales
     rollupOptions: {
       output: {
-        // Esto separa las librerías pesadas automáticamente en bloques independientes
+        // Separa automáticamente las dependencias pesadas de node_modules en bloques independientes
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
